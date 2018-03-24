3446d2cbdb362c2400b7357b464bd98b6d8a289b
khadir7 <khadirahmed.sd@gmail.com>
2018-03-24 13:32:19 +0530

    Hello world changes.

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default class App extends React.Component {
+export default class HelloWorld extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your </Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <View>
+        <Text >Hello world!</Text>
+        <Image 
+          source={{
+                uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+          }}
+          style={{width: 193, height: 110}}/>
+      </View>  
     );
   }
 }
